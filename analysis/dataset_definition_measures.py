91629d8fa7e098a4744b6b77ffc8ca1cae291976
markdrussell <100200343+markdrussell@users.noreply.github.com>
2024-08-20 20:24:03 +0100

    Snomed ctv

--- analysis/dataset_definition_measures.py
+++ analysis/dataset_definition_measures.py
@@ -1,4 +1,4 @@
-from ehrql import create_dataset, days, months, years, case, when, Measures, INTERVAL
+from ehrql import create_dataset, days, months, years, case, when, create_measures, INTERVAL
 from ehrql.tables.tpp import patients, medications, practice_registrations, clinical_events, addresses, ons_deaths, appointments
 from datetime import date
 import codelists_ehrQL as codelists
@@ -19,18 +19,36 @@ intervals = args.intervals
 index_date = INTERVAL.start_date
 end_date=INTERVAL.end_date
 
-# Prevalent diagnostic code in primary care record
-def prev_code_in_period(dx_codelist):
+# Prevalent diagnostic code in primary care record (SNOMED)
+def prev_code_in_period_snomed(dx_codelist):
     return clinical_events.where(
         clinical_events.snomedct_code.is_in(dx_codelist)
     ).where(
         clinical_events.date.is_on_or_before(index_date)
     )
 
-# Incident diagnostic code in primary care record (assuming before study end date)
-def first_code_in_period(dx_codelist):
+# Incident diagnostic code in primary care record (SNOMED) (assuming before study end date)
+def first_code_in_period_snomed(dx_codelist):
     return clinical_events.where(
         clinical_events.snomedct_code.is_in(dx_codelist)
+    ).where(
+        clinical_events.date.is_on_or_before(end_date)
+    ).sort_by(
+        clinical_events.date
+    ).first_for_patient()
+
+# Prevalent diagnostic code in primary care record (CTV3)
+def prev_code_in_period_ctv3(dx_codelist):
+    return clinical_events.where(
+        clinical_events.ctv3_code.is_in(dx_codelist)
+    ).where(
+        clinical_events.date.is_on_or_before(index_date)
+    )
+
+# Incident diagnostic code in primary care record (CTV3) (assuming before study end date)
+def first_code_in_period_ctv3(dx_codelist):
+    return clinical_events.where(
+        clinical_events.ctv3_code.is_in(dx_codelist)
     ).where(
         clinical_events.date.is_on_or_before(end_date)
     ).sort_by(
@@ -49,8 +67,28 @@ def preceding_registration(dx_date):
             practice_registrations.practice_pseudo_id,
         ).last_for_patient()
 
-#diseases = ["rheumatoid_arthritis", "axial_spondyloarthritis"]
-diseases = ["rheumatoid_arthritis"]
+# Define patient sex and date of death
+sex = patients.sex
+dod = patients.date_of_death
+
+age = patients.age_on(index_date)
+age_band = case(  
+    when((age >= 0) & (age < 18)).then("age_18_49"),
+    when((age >= 18) & (age < 50)).then("age_18_49"),
+    when((age >= 50) & (age < 65)).then("age_50_64"),
+    when((age >= 65) & (age < 75)).then("age_65_74"),
+    when((age >= 75) & (age < 85)).then("age_75_84"),
+    when((age >= 85)).then("age_greater_equal_85"),
+    otherwise="Missing",
+)
+
+diseases = ["rheumatoid_arthritis", "axial_spondyloarthritis"]
+
+dataset = create_dataset()
+
+measures = create_measures()
+measures.configure_dummy_data(population_size=1000)
+measures.define_defaults(intervals=months(intervals).starting_on(start_date))
 
 prev_numerators = {}  # Dictionary to store the numerators
 prev_denominators = {}
@@ -60,18 +98,14 @@ incidence_denominators = {}
 for disease in diseases:
 
     disease_codelist = getattr(codelists, disease)
+    column_name = disease_codelist[1]
 
-    dataset = create_dataset()
-
-    dataset.add_column(f"{disease}_prev", prev_code_in_period(disease_codelist).exists_for_patient())
-    dataset.add_column(f"{disease}_inc_date", first_code_in_period(disease_codelist).date)
-
-    # Measures
-    measures = Measures()
-
-    measures.configure_dummy_data(population_size=1000)
-
-    measures.define_defaults(intervals=months(intervals).starting_on(start_date))
+    if column_name == "code":
+        dataset.add_column(f"{disease}_prev", prev_code_in_period_snomed(disease_codelist).exists_for_patient())
+        dataset.add_column(f"{disease}_inc_date", first_code_in_period_snomed(disease_codelist).date)
+    else:
+        dataset.add_column(f"{disease}_prev", prev_code_in_period_ctv3(disease_codelist).exists_for_patient())
+        dataset.add_column(f"{disease}_inc_date", first_code_in_period_ctv3(disease_codelist).date)        
 
     ## Prevalence numerator - people currently registered on index date who have an Dx code on or before index date
     prev_numerators[f"{disease}_prev_num"] = (
@@ -94,8 +128,8 @@ for disease in diseases:
 
     ## Incidence numerator - people with new diagnostic codes in the 1 month after index date who have 12m+ prior registration
     incidence_numerators[f"{disease}_inc_num"] = (
-            (patients.age_on(getattr(dataset, f"{disease}_inc_date")) >= 0)
-            & (patients.age_on(getattr(dataset, f"{disease}_inc_date")) < 110)
+            (patients.age_on(index_date) >= 0)
+            & (patients.age_on(index_date) < 110)
             & ((patients.sex == "male") | (patients.sex == "female"))
             & (patients.date_of_death.is_after(getattr(dataset, f"{disease}_inc_date")) | patients.date_of_death.is_null())
             & (preceding_registration(getattr(dataset, f"{disease}_inc_date")).exists_for_patient())
@@ -116,12 +150,18 @@ for disease in diseases:
     measures.define_measure(
         name=disease + "_prevalence",
         numerator=prev_numerators[f"{disease}_prev_num"],
-        denominator=prev_denominators[f"{disease}_prev_denom"]
+        denominator=prev_denominators[f"{disease}_prev_denom"],
+        group_by={
+            "sex": sex,
+        },
         )
 
     # Incidence
     measures.define_measure(
         name=disease + "_incidence",
         numerator=incidence_numerators[f"{disease}_inc_num"],
-        denominator=incidence_denominators[f"{disease}_inc_denom"]
+        denominator=incidence_denominators[f"{disease}_inc_denom"],
+        group_by={
+            "sex": sex,
+        },
         )
